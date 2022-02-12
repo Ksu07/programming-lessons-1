c62828e41403560d5a357559ccba8bca0b18a937
Ksu07 <kpermenkova@yandex.ru>
2022-02-12 12:02:35 +0300

    задание2

---
+++ variables2.js
@@ -0,0 +1,17 @@
+let str = 'live';
+let k = '7';
+let mynameKsu = true;
+let age = null;
+let dream;
+let id = Symbol ('asd');
+let p = '20n';
+function qwe(){
+}
+console.log (typeof 'live');
+console.log (typeof 7);
+console.log (typeof true);
+console.log (typeof null);
+console.log (typeof dream);
+console.log (typeof id);
+console.log (typeof 20n);
+console.log (typeof qwe);
