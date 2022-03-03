46a2af333261d0642b2793b0d1af8d0470b6f7ac
Ksu07 <kpermenkova@yandex.ru>
2022-03-03 13:08:48 +0300

    создает маршрутизацию

--- практика 2.1/маршрутизация.js
+++ практика 2.1/маршрутизация.js
@@ -2,13 +2,13 @@ const http = require("http");
 const requestHandler = (request, response) => {
     response.setHeader("Content-Type", "text/html; charset=utf-8;");
     if(request.url === "/home" || request.url === "/"){
-    response.write("<h2>Home</h2>");
+    response.write("<h2>Your welcome!</h2>");
     }
     else if(request.url == "/about"){
-    response.write("<h2>hi im ks</h2>");
+    response.write("<h2>hi im Ksusha</h2>");
     }
     else if(request.url == "/contact"){
-    response.write("<h2>Contacts</h2>");
+    response.write("<h2>Telephon number: 89765434567</h2>");
     }
     else{
     response.write("<h2>Not found</h2>");
