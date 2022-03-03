2a4a8e6c562202142dd517b495df2b7e8c5aca37
Ksu07 <kpermenkova@yandex.ru>
2022-03-03 16:34:34 +0300

    применяет возможности Beautify

--- практика 2.1/маршрутизация.js
+++ практика 2.1/маршрутизация.js
@@ -1,18 +1,15 @@
 const http = require("http");
 const requestHandler = (request, response) => {
     response.setHeader("Content-Type", "text/html; charset=utf-8;");
-    if(request.url === "/home" || request.url === "/"){
-    response.write("<h2>Your welcome!</h2>");
-    }
-    else if(request.url == "/about"){
-    response.write("<h2>hi im Ksusha</h2>");
-    }
-    else if(request.url == "/contact"){
-    response.write("<h2>Telephon number: 89765434567</h2>");
-    }
-    else{
-    response.write("<h2>Not found</h2>");
+    if (request.url === "/home" || request.url === "/") {
+        response.write("<h2>Your welcome!</h2>");
+    } else if (request.url == "/about") {
+        response.write("<h2>hi im Ksusha</h2>");
+    } else if (request.url == "/contact") {
+        response.write("<h2>Telephon number: 89765434567</h2>");
+    } else {
+        response.write("<h2>Not found</h2>");
     }
     response.end();
-    };
-    http.createServer(requestHandler).listen(3000);
+};
+http.createServer(requestHandler).listen(3000);
